8b786698c29bc8bcaa54dd4875fb92edb655f21d
Slothdemon22 <162999948+Slothdemon22@users.noreply.github.com>
2024-08-31 20:30:46 +0500

    cors1

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
 
     const res = NextResponse.next();
-    res.headers.set("Access-Control-Allow-Origin", "*");
+    res.headers.set("Access-Control-Allow-Origin", "https://main.d1qe4cj838e38d.amplifyapp.com");
     return res;
 
 
