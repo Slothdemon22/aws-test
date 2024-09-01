82144bdaf8cd458ed1a91724ec4f736cb8b4b853
Slothdemon22 <162999948+Slothdemon22@users.noreply.github.com>
2024-09-01 16:32:27 +0500

    images

--- src/comp/middlePage.tsx
+++ src/comp/middlePage.tsx
@@ -21,50 +21,11 @@ const MiddlePage = () => {
 
   return (
     <>
-      <section
-        ref={containerRef}
-        className="flex w-full h-[50vh] rounded-lg border-4 border-dashed border-gray-300 overflow-hidden"
-      >
-        {/* Nutrition Image Container */}
-        <div className="flex items-center justify-center p-2 w-1/2 bg-gray-400">
-          <Image
-            src="/nutricition.png"
-            alt="Nutrition"
-            className="object-contain w-full h-auto"
-            quality={100}
-            width={300}
-            height={300}
-            sizes="100vw"
-          />
-        </div>
-        
-        {/* Content Container */}
-        <div className="w-1/2 h-full flex flex-col p-2">
-          {/* Healthy Habits Image */}
-          <div className="flex-1 flex items-center justify-center p-2 border-b border-gray-300">
-            <Image
-              src="/healthyhabits.png"
-              alt="Healthy Habits"
-              className="object-cover w-full h-full"
-              quality={100}
-              width={300}
-              height={300}
-              sizes="100vw"
-            />
-          </div>
-          {/* Happy Customers Image */}
-          <div className="flex-1 flex items-center justify-center p-2">
-            <Image
-              src="/happycustomers.png"
-              alt="Happy Customers"
-              className="object-cover w-[80%] h-[70%] max-w-[200px] max-h-[200px]"
-              quality={100}
-              width={200}
-              height={200}
-              sizes="100vw"
-            />
-          </div>
-        </div>
+          <section className='md:w-[80%] w-full h-[100%] '>
+              <div className='w-full'>
+                  
+                  <Image src="/base.png" alt="hero image" width={1500} height={900} className="w-full h-auto" quality={100} sizes="100vw" />
+              </div>
       </section>
     </>
   );
